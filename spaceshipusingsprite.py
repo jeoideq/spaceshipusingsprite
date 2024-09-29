e26558a75c7272d60231dd3b5731895edfb8e234
jeoideq <hanssonj20@gmail.com>
2024-09-29 12:57:19 +0200

    Add files via upload

--- spaceshipusingsprite.py
+++ spaceshipusingsprite.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.init()
 
 WIDTH=600
 HEIGHT=600 
@@ -8,12 +9,58 @@ screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption(TITLE)
 
 background=pygame.image.load("space.png")
+spaceship=pygame.image.load("spaceship.png")
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self,x,y,image):
+        super().__init__()
+        self.image=image
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+
+
+rocket=Spaceship(300,300,spaceship)
+sprites=pygame.sprite.Group()
+sprites.add(rocket)
+
+
+        
+
+
+
+        
 
 run = True 
 while run:
     screen.blit(background, (0,0))
-    for event in pygame.event.get():
+    sprites.draw(screen)
+    keys=pygame.key.get_pressed()
+    if keys [pygame.K_w]:
+        rocket.rect.y +=5
+    if keys [pygame.K_s]:
+        rocket.rect.y -=5
+    if keys [pygame.K_a]:
+         rocket.rect.x +=5
+    if keys [pygame.K_d]:
+        rocket.rect.x -=5
+    if rocket.rect.left<0:
+        rocket.rect.left=0
+    if rocket.rect.right>600:
+        rocket.rect.right=600
+    if rocket.rect.top<0:
+        rocket.rect.top=0
+    if rocket.rect.bottom>600:
+        rocket.rect.bottom=600
         
+    
+
+    
+
+
+
+
+    for event in pygame.event.get():  
         if event.type == pygame.QUIT:
             run = False
 
